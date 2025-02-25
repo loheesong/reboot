34e40a633abc0f75dbd03ef265b4e69e6a1d054f
loheesong <loheesong@gmail.com>
2025-02-24 19:29:38 -0800

    fix: player only jump on floors

---
+++ Assets/FeetCollider.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FeetCollider : MonoBehaviour
+{
+    bool isFloor(GameObject gameObject) {
+        return gameObject.layer == LayerMask.NameToLayer("Floor");
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponentInParent<PlayerController>().canJump = isFloor(collision.gameObject);
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        GetComponentInParent<PlayerController>().canJump = false;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,24 @@
+using Unity.Burst.Intrinsics;
 using UnityEngine;
+using UnityEngine.InputSystem.XInput;
 
 public class PlayerController : MonoBehaviour
 {
     #region Movement_variables
     public float moveSpeed = 3;
+    public float jumpHeight = 300;
+    public float maxSpeed = 1000;
     float x_input;
+    public bool canJump;
 
     #endregion
 
     #region Physics_variables
     Rigidbody2D rb;
+    #endregion
+
+    #region Gameplay_variables
+    private bool hasKey;
     #endregion
 
     void Awake()
@@ -30,19 +39,29 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    #region Movement_functions
     private void Move()
     {   
         x_input = Input.GetAxisRaw("Horizontal");
-        Vector2 vector = Vector2.zero;
-        if (x_input == -1) {
-            vector = Vector2.left;
-        } else if (x_input == 1) {
-            vector = Vector2.right;
-        }
-        rb.linearVelocityX = vector.x * moveSpeed;
+        rb.AddForce(new Vector2(x_input * moveSpeed, 0));
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            rb.AddForce(new Vector2(0,100));
+        if (rb.linearVelocity.x > maxSpeed) {
+			rb.linearVelocity = new Vector2 (maxSpeed, rb.linearVelocity.y);
+		}
+		if (rb.linearVelocity.x < -maxSpeed) {
+			rb.linearVelocity = new Vector2 (-maxSpeed, rb.linearVelocity.y);
+		}
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump) {
+            rb.AddForce(new Vector2(0, jumpHeight));
         }
     }
+
+    #endregion
+
+    #region Gameplay_functions
+    public void CollectedKey() {
+        hasKey = true;
+    }
+    #endregion
 }
