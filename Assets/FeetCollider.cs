fb89564367d038240558c992353e442196a8e436
noemi <noemi.chulo@berkeley.edu>
2025-02-25 18:11:27 -0800

    started work on door

--- Assets/FeetCollider.cs
+++ Assets/FeetCollider.cs
@@ -1,13 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FeetCollider : MonoBehaviour
 {
     bool isFloor(GameObject gameObject) {
-        return gameObject.layer == LayerMask.NameToLayer("Floor");
+        //return gameObject.layer == LayerMask.NameToLayer("Floor");
+        Debug.Log("jump");
+        return gameObject.tag == "Floor";
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // Check to see if player hit the floor so they can get jump perms back
         GetComponentInParent<PlayerController>().canJump = isFloor(collision.gameObject);
+        // Check to see if door exit, leave scene
+        if (collision.gameObject.tag == "Exit") {
+            string scenename = SceneManager.GetActiveScene().name;
+            string newSceneNum = (((int) scenename[scenename.Length - 1]) + 1).ToString();
+            Debug.Log("Exit to next scene " + newSceneNum);
+            SceneManager.LoadScene("Level" + newSceneNum);}
     }
 
     void OnCollisionExit2D(Collision2D collision)
