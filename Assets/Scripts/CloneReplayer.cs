b8b10d36860b7330b03e2bbe32f7c1563547c28f
Xavier Tan <164714627+XavierTann@users.noreply.github.com>
2025-03-01 09:24:40 -0800

    clone animation

--- Assets/Scripts/CloneReplayer.cs
+++ Assets/Scripts/CloneReplayer.cs
@@ -1,45 +1,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloneReplayer : MonoBehaviour {
+public class CloneReplayer : MonoBehaviour
+{
     private List<PlayerController.RecordedFrame> replayData;
     private int frameIndex = 0;
     private Rigidbody2D rb;
     private float startTime;
 
     public LayerMask interactable;
-    
-    public void Initialize(List<PlayerController.RecordedFrame> data) {
+
+    private Animator anim;
+
+    public void Initialize(List<PlayerController.RecordedFrame> data)
+    {
         replayData = data;
         rb = GetComponent<Rigidbody2D>();
         rb.bodyType = RigidbodyType2D.Kinematic; // prevent interactions with other clones
         startTime = Time.time;
+        anim = GetComponent<Animator>();
     }
 
-    void FixedUpdate() {
-        if (frameIndex >= replayData.Count) {
+    private void Start()
+    {
+        anim.SetFloat("facingDirection", 1);
+    }
+
+    void FixedUpdate()
+    {
+        if (frameIndex >= replayData.Count)
+        {
             Destroy(gameObject);
             return;
         }
 
         float elapsedTime = Time.time - startTime;
 
-        while (frameIndex < replayData.Count && replayData[frameIndex].time <= elapsedTime) {
+        while (frameIndex < replayData.Count && replayData[frameIndex].time <= elapsedTime)
+        {
             frameIndex++;
         }
 
-        if (frameIndex < replayData.Count) {
+        if (frameIndex < replayData.Count)
+        {
             rb.MovePosition(replayData[frameIndex].position);
             rb.rotation = replayData[frameIndex].rotation;
             rb.linearVelocity = replayData[frameIndex].velocity;
         }
 
-        // need to fix later to interact with boxes 
-        Collider2D[] detectedPlates = Physics2D.OverlapBoxAll(transform.position, rb.GetComponent<BoxCollider2D>().bounds.size, 0, interactable);
+        // ANIMATION
+        // anim.SetBool("isJumping", !canJump);
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        if (rb.linearVelocity.x != 0)
+        {
+            anim.SetFloat("facingDirection", rb.linearVelocity.x);
+        }
+
+        // ANIMATION END
+
+        // need to fix later to interact with boxes
+        Collider2D[] detectedPlates = Physics2D.OverlapBoxAll(
+            transform.position,
+            rb.GetComponent<BoxCollider2D>().bounds.size,
+            0,
+            interactable
+        );
         foreach (Collider2D plate in detectedPlates)
         {
             plate.GetComponent<Button>().Activate(this.GetComponent<Collider2D>());
         }
-
     }
 }
